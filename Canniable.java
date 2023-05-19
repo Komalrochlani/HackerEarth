91114a3ecc50a60d50d4c644e7141d2e7b03f2b9
Komalrochlani <komalrochlani1994@gmail.com>
2023-05-20 00:33:59 +0530

    updated

--- Canniable.java
+++ Canniable.java
@@ -8,43 +8,6 @@ public class Canniable {
 		System.out.println(Minimum_Operations(s.length(), s));
 	}
 
-/*	static int Minimum_Operations(int n, String s) {
-		// Write your code here
-		int result = 0;
-		StringBuilder sb = new StringBuilder(s);
-		// baa
-		while (n > 1) {
-			char ch = sb.charAt(1);
-			boolean flagL = false;
-			boolean flagR = false;
-			// int lastChar = sb.lastIndexOf(ch + "");
-			for (int i = 0; i < n; i++) {
-				if (sb.charAt(i) == ch) {
-					sb.deleteCharAt(i);
-					n--;
-					flagL = true;
-					break;
-				}
-			}
-
-			for (int i = n - 1; i > 0; i--) {
-				if (sb.charAt(i) == ch) {
-					sb.deleteCharAt(i);
-					n--;
-					flagR = true;
-					break;
-				}
-			}
-			
-			if (flagL && flagR) {
-				result++;
-			}
-			System.out.println(sb.toString());
-		}
-
-		return result;
-
-	}*/
 	
 	static int Minimum_Operations(int n, String s){
 	    // Write your code here
